dc13750e17a7ff6c8ef75431349d6c216237ac3c
SARTHAK <shibugarg0303@gmail.com>
2022-11-20 23:46:27 +0530

    change

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,23 +10,23 @@ const Contact = () => {
                     <div className="row row-30">
                         <div className="col-md-4 col-xl-5">
                             <div className="pr-xl-4">
-                                <p>We are an award-winning creative agency, dedicated to the best result in web design, promotion, business consulting, and marketing.</p>
-                                <p className="rights"><span>©  </span><span className="copyright-year">2018</span><span> </span><span>Waves</span><span>. </span><span>All Rights Reserved.</span></p>
+                                <p>It is a personal website created by Sarthak from scratch.</p>
+                                <p className="rights"><span>©  </span><span className="copyright-year">2022</span><span> </span><span>. </span><span>All Rights Reserved.</span></p>
                             </div>
                         </div>
                         <div className="col-md-4">
                             <h5>Contacts</h5>
                             <dl className="contact-list">
                                 <dt>Address:</dt>
-                                <dd>798 South Park Avenue, Jaipur, Raj</dd>
+                                <dd>Dehradun, Uttarakhand</dd>
                             </dl>
                             <dl className="contact-list">
                                 <dt>email:</dt>
-                                <dd><a href="mailto:#">dkstudioin@gmail.com</a></dd>
+                                <dd><a rel="noreferrer" target="_blank" href="mailto:#">shibugarg0303@gmail.com</a></dd>
                             </dl>
                             <dl className="contact-list">
                                 <dt>phones:</dt>
-                                <dd><a href="tel:#">https://karosearch.com</a> <span>or</span> <a href="tel:#">https://karosearch.com</a>
+                                <dd><a rel="noreferrer" target="_blank" href="tel:#">+91 9368122660</a>
                                 </dd>
                             </dl>
                         </div>
@@ -43,10 +43,11 @@ const Contact = () => {
                     </div>
                 </div>
                 <div className="row no-gutters social-container">
-                    <div className="col"><a className="social-inner" href="#"><span>Facebook</span></a></div>
-                    <div className="col"><a className="social-inner" href="#"><span>instagram</span></a></div>
-                    <div className="col"><a className="social-inner" href="#"><span>twitter</span></a></div>
-                    <div className="col"><a className="social-inner" href="#"><span>Github</span></a></div>
+                    <div className="col"><a rel="noreferrer" target="_blank" className="social-inner" href="https://www.facebook.com/"><span>Facebook</span></a></div>
+                    <div className="col"><a rel="noreferrer" target="_blank" className="social-inner" href="https://www.instagram.com/_sarthak__003/"><span>instagram</span></a></div>
+                    <div className="col"><a rel="noreferrer" target="_blank" className="social-inner" href="https://twitter.com/?lang=en-in"><span>twitter</span></a></div>
+                    {/* // eslint-disable-next-line */}
+                    <div className="col"><a rel="noreferrer" target="_blank" className="social-inner" href="https://github.com/Sar-thak-3"><span>Github</span></a></div>
                 </div>
             </footer>
         </div>
